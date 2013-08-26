66b515af5ce1fd4463a1d7ce93861dd75b0346b1
Jairo Diaz <jairo.diaz@codescrum.com>
2013-08-26 16:51:43 +0100

    Completed feature searching for venues

--- ruby/spec/integration/searching_venues_spec.rb
+++ ruby/spec/integration/searching_venues_spec.rb
@@ -1,11 +1,23 @@
 require 'spec_helper'
 
-feature 'Searching for a venue', :type => :feature do
+feature 'Searching for a venue' do
 
   scenario 'Visiting the homepage' do
     visit '/'
     expect(page).to have_content 'Your venues'
     expect(page.find('input#query').text).to eq ''
     expect(page).to have_selector :link_or_button, 'search'
+  end
+
+  scenario 'Searching for a keyword' do
+    visit '/'
+    fill_in 'query', with: 'video'
+    VCR.use_cassette('video') do
+      click_button 'search'
+    end
+
+    expect(page).to have_content 'Matching Venues'
+    expect(page).to have_content 'Video City'
+    expect(page).to have_content 'Music & Video Exchange'
   end
 end
--- ruby/venues_app.rb
+++ ruby/venues_app.rb
@@ -1,10 +1,37 @@
+$: << File.join(File.dirname(__FILE__), ".", "lib")
+
 require 'sinatra'
+require 'venue_finder'
+require 'awesome_print'
 
 helpers do
   include Rack::Utils
   alias_method :h, :escape_html
+
+  def checker(boolean)
+    'checked' if boolean
+  end
+
+  def address(address)
+    begin
+      location = "#{address['address']}&nbsp;#{address['postalCode']}"
+    rescue
+     "&nbsp;"
+    end
+  end
 end
 
 get '/' do
   erb :index
+end
+
+post '/' do
+  if params[:query] and not params[:query].empty?
+    @venues = VenueFinder.instance.find current_user, params[:query]
+  end
+  erb :index
+end
+
+def current_user
+  '1'
 end
